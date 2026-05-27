de96f0adea2c35ae693052aec5023d9eca750f8f
niggz96 <tahdasilva@gmail.com>
2026-05-27 15:10:38 -0300

    Main com os  objetos
    Incluído três classes de personagens para o game Guardiões e também a função Exibir status

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+        // Criando um objeto da classe Personagem
+        Personagem heroi1 = new Personagem("João", "Guerreiro", 5, 100, 10.0);
+        Personagem heroi2 = new Personagem("Maria", "Maga", 3, 80, 15.0);
+        Personagem heroi3 = new Personagem("Carlos", "Arqueiro", 4, 90, 12.0);
 
-        .
-        .
-        .
-
+        // Exibindo as informações do personagem
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
+        heroi3.exibirStatus();
     }
 }
